fa7fc4715af9b0251bc722fd76e7bec27402c5bd
Anastasia Chemko <nast.chemko@gmail.com>
2022-02-23 17:34:59 +0300

    add test with owner

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -2,8 +2,10 @@ package tests;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
+import config.CredentialsConfig;
 import helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.aeonbits.owner.ConfigFactory;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.openqa.selenium.remote.DesiredCapabilities;
@@ -16,11 +18,14 @@ public class TestBase {
     static void setUp() {
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
 
+        CredentialsConfig config = ConfigFactory.create(CredentialsConfig.class);
+
+        String login = config.login();
+        String password = config.password();
+
         String browser = System.getProperty("browser", "chrome");
         String version = System.getProperty("version", "90");
         String remoteUrl = System.getProperty("remoteUrl", "selenoid.autotests.cloud/wd/hub");
-        String login = System.getProperty("login", "user1");
-        String password = System.getProperty("password", "1234");
 
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserSize = "1920x1080";
